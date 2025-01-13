05714a86911bd58d2954620db2d980232746ab1d
saffi <maharjansaffi@gmail.com>
2025-01-14 00:34:04 +0545

    using pkg-up logic

---
+++ tool/src/commands/start.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+ module.export=function start(config){
+    console.log(chalk.bgBlueBright('starting the app'));
+    console.log(chalk.gray('Recieved configuration'))
+ }
